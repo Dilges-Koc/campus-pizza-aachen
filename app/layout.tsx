3f6d6e4d4e50275b84cadbb1420b91dbbb303857
Dilges-Koc <dilgaskoc@outlook.de>
2026-05-05 19:13:06 +0200

    Update metadata base

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://https://campus-pizza-aachen.vercel.app"),
   title: "Campus Pizza Aachen",
-  description: "Campus Pizza in Aachen",
+  description:
+    "Campus Pizza in Aachen: Jede Pizza 5,00 €, 3 Zutaten nach Wahl, 29 cm. Speisekarte, Öffnungszeiten und Kontakt auf einen Blick.",
 };
 
 export default function RootLayout({
